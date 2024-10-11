44c46a7bb517cae679808f318c6d138aa0427f10
913122104033-DineshKumar <dineshkumar12a106.2122@gmail.com>
2024-10-12 04:15:15 +0530

    Changes made in files are saved

--- app/src/main/java/com/example/fixup/apicalls/RetroFitClient.java
+++ app/src/main/java/com/example/fixup/apicalls/RetroFitClient.java
@@ -1,4 +1,17 @@
 package com.example.fixup.apicalls;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetroFitClient {
+    private static Retrofit retrofit = null;
+    public static Retrofit getClient(String baseUri) {
+        if(retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(baseUri)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
